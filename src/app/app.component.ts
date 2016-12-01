4616262c22d9086eaef04386f344a94476c62f57
pugovka <ms.n.yunak@gmail.com>
2016-12-01 15:16:07 +1000

    Init application

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import '../assets/css/styles.css';
+
+@Component({
+  selector: 'pugovka-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent { 
+  title = 'ToDo list'; 
+}
